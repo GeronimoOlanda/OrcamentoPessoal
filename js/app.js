57d53b2cddfc1130e4de7330e0c3a9d4566fa2ba
GeronimoOlanda <51466744+GeronimoOlanda@users.noreply.github.com>
2020-07-08 05:45:05 -0300

    Convertendo Objetos Literais para JSON
    Fazendo a conversão para JSON

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ class Despesa{
     }
 }
 let cadastrarDespesa = () =>{
+    //recuperando os valores do formulario html
     let ano = document.getElementById('ano').value
     let mes = document.getElementById('mes').value
     let dia = document.getElementById('dia').value
@@ -16,7 +17,15 @@ let cadastrarDespesa = () =>{
     let tipo =  document.getElementById('tipo').value
     let descricao = document.getElementById('descricao').value
     let valor = document.getElementById('valor').value
+    
+    //Instanciando a Classe Despesa()
+     let despesa = new Despesa(ano,mes,dia,tipo,descricao,valor) 
 
- let despesa = new Despesa(ano,mes,dia,tipo,descricao,valor) 
- console.log(despesa) 
+    console.log(despesa) 
+    gravar(despesa)
+}
+//stringify() converte um objeto literal para JSON
+//JSON.parse() converte um JSON para objeto Literal
+let gravar = (d) =>{
+    localStorage.setItem('despesa',JSON.stringify(d))
 }
